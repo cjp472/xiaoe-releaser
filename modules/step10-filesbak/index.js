4d187206c73df294b3b20eb86f62ed4be6b1c6b6
qianlipp <yuqianliwb@gmail.com>
2017-06-14 17:14:43 +0800

    init

--- modules/step10-filesbak/index.js
+++ modules/step10-filesbak/index.js
@@ -1,10 +1,9 @@
 /**
  * Created by qianlipp on 2017/6/14.
  */
-var filesbak = require("./modules/step10-filesbak/index");
 
 module.exports = {
     test: function () {
-        filesbak.test();
+        console.log("step10-filesbak test!");
     }
 };
---
+++ test.js
@@ -0,0 +1,6 @@
+/**
+ * Created by qianlipp on 2017/6/14.
+ */
+var tool = require("./index");
+
+tool.test();
