f5630fc16d1eb4b854e5817daada27d4f87de73c
qianlipp <yuqianliwb@gmail.com>
2017-06-14 19:34:25 +0800

    init

--- modules/_libs/utils.js
+++ modules/_libs/utils.js
@@ -1,6 +1,7 @@
 /**
  * Created by qianlipp on 2017/6/14.
  */
+var process = require('child_process');
 
 /**
  * 是否以指定字符结尾
@@ -14,6 +15,11 @@ var isEndWith = function (srcStr, dstStr) {
     return (srcStr.indexOf(dstStr) + dstStr.length) == srcStr.length;
 };
 
+var runShell = function (shell) {
+    process.exec(shell);
+};
+
 module.exports = {
-    isEndWith: isEndWith
+    isEndWith: isEndWith,
+    runShell: runShell
 };
--- modules/step10-prepare/index.js
+++ modules/step10-prepare/index.js
@@ -4,6 +4,7 @@
  * 1、如果已经是在线状态，则恢复
  * 2、将要修改的文件拷贝到其他文件夹，备份，防止异常
  */
+var fs = require("fs");
 var utils = require("../_libs/utils");
 
 var basePath = null;
@@ -22,7 +23,12 @@ var run = function (base_path) {
  * 恢复到初始状态
  */
 var revertStatus = function () {
+    if (fs.existsSync(basePath + "public/ONLINE_VERSION_LOCK")) {
+        //如果存在现网锁，说明是现网版本，先恢复到标准版本
 
+    }
+    //备份public目录
+    utils.runShell("mv " + basePath + "public " + basePath + "public_bak")
 };
 
 module.exports = {
